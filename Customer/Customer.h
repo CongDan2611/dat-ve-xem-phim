8de28418fc986ade331394c28f90d1ddd0cfe6c5
Tấn Trúc <tantrucdev@gmail.com>
2026-09-12 20:44:20 +0700

    customer

---
+++ Customer/Customer.h
@@ -0,0 +1,26 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include "../Person/Person.h"
+
+using namespace std;
+
+class Customer : public Person {
+private:
+    int loyaltyPoints;
+
+public:
+    Customer();
+    Customer(const string& id, const string& name,
+             const string& phone, const string& email,
+             int loyaltyPoints = 0);
+
+    int getLoyaltyPoints() const;
+    void setLoyaltyPoints(int points);
+    void addPoints(int points);
+
+    void input() override;
+    void displayInfo() const override;
+    string getType() const override;
+};
+#endif
