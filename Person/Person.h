87c946841356c31d8264e47d7854fbfb0bee2083
Tấn Trúc <tantrucdev@gmail.com>
2026-09-12 20:43:54 +0700

    Class Person

---
+++ Person/Person.h
@@ -0,0 +1,34 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+
+using namespace std;
+
+class Person {
+protected:
+    string id;
+    string name;
+    string phone;
+    string email;
+
+public:
+    Person();
+        Person(const string& id, const string& name,
+            const string& phone, const string& email);
+    virtual ~Person() = default;
+
+    string getId() const;
+    string getName() const;
+    string getPhone() const;
+    string getEmail() const;
+
+    void setName(const string& name);
+    void setPhone(const string& phone);
+    void setEmail(const string& email);
+
+    virtual void input();
+    virtual void displayInfo() const = 0;
+    virtual string getType() const = 0;
+};
+#endif
